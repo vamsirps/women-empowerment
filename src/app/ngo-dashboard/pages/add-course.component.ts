38e47bec2bb88b5fa60414dc4e024a30c3602c78
vamsirps <110928068+vamsirps@users.noreply.github.com>
2022-08-10 11:53:18 +0530

    files upload

---
+++ src/app/ngo-dashboard/pages/add-course.component.ts
@@ -0,0 +1,43 @@
+import { Component } from '@angular/core';
+import { Courses } from 'src/app/step/pages/courses';
+import { AddCourseService } from './add-course-service';
+
+@Component({
+    selector: 'add-course',
+    templateUrl: './add-course.component.html',
+    styleUrls: ['../../css/bootstrap.min.css','../../css/mdb.min.css','./add-course.component.css']
+})
+
+export class AddCourseComponent {
+    //course:TrainingCourse[];
+    course: Courses = new Courses();
+    response: string;
+    
+
+    constructor(public ms: AddCourseService) {
+
+    }
+
+    add(mform){
+        this.ms.sendToServer(this.course).subscribe(
+            data => {
+                //Take the response from server and storing in string variable
+                this.response = data['status'];
+                this.reloadPage();
+            }
+        );
+    }
+
+    keyPress(event: any) {
+        const pattern = /[0-9\+\-\ ]/;
+        let inputChar = String.fromCharCode(event.charCode);
+        if (event.keyCode != 8 && !pattern.test(inputChar)) {
+            event.preventDefault();
+        }
+    }
+    
+    
+    reloadPage() {
+        window.location.href = './ngo/success';
+    }
+}
---
+++ src/app/user-dashboard/pages/add-course.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+import { UserCourse } from './user-course';
+import { AddCourseService } from './add-course-service';
+import { Login } from 'src/app/login';
+
+@Component({
+    selector: 'add-user-course',
+    templateUrl: './add-course.component.html',
+    styleUrls: ['./add-course.component.css', '../../css/bootstrap.min.css', '../../css/mdb.min.css']
+})
+
+export class AddCourseComponent {
+
+    response: string;
+
+    ngOnInit() {
+       
+    }
+
+    usercourse: UserCourse = new UserCourse();
+    constructor(private us: AddCourseService) {
+
+    }
+
+    keyPress(event: any) {
+        const pattern = /[0-9\+\-\ ]/;
+        let inputChar = String.fromCharCode(event.charCode);
+        if (event.keyCode != 8 && !pattern.test(inputChar)) {
+            event.preventDefault();
+        }
+    }
+    
+
+    add() {
+        this.us.sendToServer(this.usercourse).subscribe(
+            data => {
+                //Take the response from server and storing in string variable
+                this.response = data['status'];
+            }
+        );
+    }
+
+
+}
